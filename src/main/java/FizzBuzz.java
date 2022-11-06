6417ffbf547fbc742e2724ed819696d8d35a9d39
sabo <sabo@jihyes-mbp.home>
2022-11-06 11:44:24 +0000

    refactoring method 'of'

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,16 +1,20 @@
 public class FizzBuzz {
 
     public static String of(int i) {
+        String fizzBuzzResult = "";
+
         if(i == 0) {
-            return "0";
+            fizzBuzzResult = "0";
         } else if( (i % 3 == 0) && (i % 5 == 0) ) {
-            return "FizzBuzz";
+            fizzBuzzResult = "FizzBuzz";
         } else if(i % 3 == 0) {
-            return "Fizz";
+            fizzBuzzResult =  "Fizz";
         } else if(i % 5 == 0) {
-            return "Buzz";
+            fizzBuzzResult = "Buzz";
+        } else {
+            fizzBuzzResult = String.valueOf(i);
         }
 
-        return String.valueOf(i);
+        return fizzBuzzResult;
     }
 }
